0c2bf42fc035686023090aa37f1d5238fa8f4bf1
LoreviQ <lorevi@protonmail.com>
2024-02-20 01:05:14 +0000

    Added method to add a user to DB

--- db_client.py
+++ db_client.py
@@ -1,12 +1,13 @@
 """Connects to MongoDB Database"""
 
+import datetime
 import os
 
 import pymongo
 from dotenv import load_dotenv
 
 
-class DB(pymongo.MongoClient):
+class MDBclient(pymongo.MongoClient):
     """
     Creates a subclass of the pymongo.MongoClient class
     Connects to MongoDB database based on .env variables
@@ -16,7 +17,39 @@ class DB(pymongo.MongoClient):
         load_dotenv()
         super().__init__( f"mongodb://{os.getenv("MONGODB_ADDRESS")}:27017/")
 
+    def add_user(self, uname, platform):
+        """
+        Adds a user to the database
+
+        Args:
+            uname: Username of the user (String)
+            platform: Name of the platform (String)
+
+        Returns:
+            This is a description of what is returned.
+
+        Raises:
+            KeyError: Raises an exception.
+        """
+        collection = self['ChatDB']['user']
+        if platform == "Twitch":
+            if collection.find_one({"twitch_uname": uname}):
+                print("User already exists")
+            else:
+                user = {
+                    "user": uname,
+                    "twitch_uname": uname,
+                    "created_date": datetime.datetime.now(tz=datetime.timezone.utc),
+                }
+                collection.insert_one(user)
+                print("Added user")
+        else:
+            raise ValueError("Unsupported Platform")
+
 
 if __name__ == "__main__":
-    db = DB()
-    print(db.list_database_names())
+    db = MDBclient()
+    db.add_user("test101", "Twitch")
+    db.add_user("test101", "Twitch")
+    for document in db['ChatDB']['user'].find():
+        print(document)
